21ad63c49ab74f8ce8e0d477906393fdd225ab7d
Adarsh M M <vvce23cse0234@vvce.ac.in>
2025-11-10 03:26:31 +0530

    Make some styles updates.

--- frontend/src/Pages/History.jsx
+++ frontend/src/Pages/History.jsx
@@ -13,7 +13,7 @@ export default function History() {
         <div className="min-h-screen p-4 max-w-5xl mx-auto">
             {/* Header */}
             <div className="flex items-center justify-between mb-5">
-                <h2 className="text-2xl font-bold text-slate-900">Quiz History</h2>
+                <h2 className="text-2xl font-bold text-slate-50">Quiz History</h2>
                 <button
                     onClick={() => navigate("/")}
                     className="bg-slate-700 hover:bg-slate-600 text-slate-100 px-4 py-2 rounded-lg shadow transition"
